1e751527e6130ce899bae6c92510a2c7fffe6c0a
Alex Kempf <ackempf@eagles.usi.edu>
2017-07-14 15:03:34 -0500

    player uses emojis if it can

--- commands/player/player.js
+++ commands/player/player.js
@@ -52,7 +52,7 @@ module.exports = class StatsCommand extends Commando.Command {
     constructor(client) {
         super(client, {
             name: 'player',
-            aliases: ['getclan','details'],
+            aliases: ['getclan', 'details'],
             group: 'player',
             memberName: 'player',
             description: 'Get players details',
@@ -77,7 +77,7 @@ module.exports = class StatsCommand extends Commando.Command {
                 this.createEmbed(result).then(embed => {
                     message.embed(embed);
                 });
-                console.log('err',err);
+                console.log('err', err);
             })
         }).catch(err => {
             console.log('err', err);
@@ -88,19 +88,18 @@ module.exports = class StatsCommand extends Commando.Command {
         const p = result[0];
         var ava = `https://secure.runescape.com/m=avatar-rs/'${p.name.trim().replace(/ /g, '%20')}/chat.png?timestamp=${new Date().getTime()}`;
         let embed = new RichEmbed()
-            .setAuthor(`${p.isSuffix ? p.name.trim().toTitleCase() + ' ' + p.title : p.title + ' ' + p.name.trim().toTitleCase()}`,'http://vignette3.wikia.nocookie.net/runescape2/images/8/88/RuneMetrics_icon.png/revision/latest?cb=20160421161211',`https://apps.runescape.com/runemetrics/app/overview/player/${p.name.trim().replace(/ /g, '%20')}`)
+            .setAuthor(`${p.isSuffix ? p.name.trim().toTitleCase() + ' ' + p.title : p.title + ' ' + p.name.trim().toTitleCase()}`, 'http://vignette3.wikia.nocookie.net/runescape2/images/8/88/RuneMetrics_icon.png/revision/latest?cb=20160421161211', `https://apps.runescape.com/runemetrics/app/overview/player/${p.name.trim().replace(/ /g, '%20')}`)
             .setTimestamp()
             .setThumbnail(ava)
             .addField(`Ingame Status`.mdbold(), `${p.online ? p.world : 'Offline'}`, true)
             .addField(`Clan`.mdbold(), `${p.clan ? p.clan : 'No Clan'}`, true);
         if (typeof profile !== 'undefined' && typeof profile.error === 'undefined') {
-            let rank = typeof emojis !== 'undefined' && typeof emojis.get('Rank') !== 'undefined' ? emojis.get('Rank') : '**Rank:**';
-            let overall = typeof emojis !== 'undefined' && typeof emojis.get('Overall') !== 'undefined' ? emojis.get('Overall') : '**Level:**';
-            let xp = typeof emojis !== 'undefined' && typeof emojis.get('Xp') !== 'undefined' ? emojis.get('Xp') : '**XP:**';
-            embed.setDescription(`${rank} ${profile.rank}\n${overall} ${profile.totalskill}\n${xp} ${profile.totalxp.toLocaleString()}`);
+            embed.setDescription(`${ typeof emojis !== 'undefined' && typeof emojis.find(item => item.name == 'overall'.toTitleCase()) !== 'undefined' ? emojis.find(item => item.name == 'overall'.toTitleCase()) : 'overall'.toTitleCase()} ${profile.totalskill.toLocaleString().mdbold()}\n` +
+                `${ typeof emojis !== 'undefined' && typeof emojis.find(item => item.name == 'xp'.toTitleCase()) !== 'undefined' ? emojis.find(item => item.name == 'xp'.toTitleCase()) : 'xp'.toTitleCase()} ${profile.totalxp.toLocaleString()}\n` +
+                `${ typeof emojis !== 'undefined' && typeof emojis.find(item => item.name == 'rank'.toTitleCase()) !== 'undefined' ? emojis.find(item => item.name == 'rank'.toTitleCase()) : 'rank'.toTitleCase()} ${profile.rank.toLocaleString()}`)
             if (typeof profile.activities !== 'undefined' && profile.activities.length > 4) {
                 let s = '';
-                profile.activities.splice(0,5).forEach(function(activity) {
+                profile.activities.splice(0, 5).forEach(function (activity) {
                     s += `${activity.text.toLowerCase().indexOf('levelled') > -1 ? activity.details : activity.text}\n`
                 });
                 embed.addField('Recent Achievements', s, false);
