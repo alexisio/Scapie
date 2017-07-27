c8674c7d258f25a080f11797a70788ccec7c4136
Alex Kempf <ackempf@eagles.usi.edu>
2017-07-27 13:54:40 -0500

    changed raven from thumbnail to author picture

--- commands/minigames/raven.js
+++ commands/minigames/raven.js
@@ -18,8 +18,7 @@ module.exports = class RavenCommand extends Commando.Command {
     async run(message, args) {
         util.request.api(`/api/minigames/raven`).then(raven => {
             let embed = new RichEmbed()
-                .setAuthor(`Raven`, ``, ``)
-                .setThumbnail(`https://vignette1.wikia.nocookie.net/runescape2/images/d/d5/Raven_%28Prifddinas%29.png/revision/latest/scale-to-width-down/200?cb=20141004102306`)
+                .setAuthor(`Raven`, `https://vignette1.wikia.nocookie.net/runescape2/images/d/d5/Raven_%28Prifddinas%29.png/revision/latest/scale-to-width-down/200?cb=20141004102306`, ``)
                 .setTimestamp()
                 .setDescription(`${raven.isSpawned ? 'There is currently a raven spawned in Prifddinas.' : `There is not a raven spawned in Prifddinas at this time.\n` +
                     `One will spawn in ${raven.daysUntilNext.toString().mdbold()} ${raven.daysUntilNext > 1 ? 'days'.mdbold() : 'day'.mdbold()}.`}`);
