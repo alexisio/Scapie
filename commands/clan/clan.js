786cecfd311259da235a79302820ebb5917d6d66
Alex Kempf <ackempf@eagles.usi.edu>
2018-03-26 17:16:01 -0500

    Add clan command for drake to use once a year

---
+++ commands/clan/clan.js
@@ -0,0 +1,58 @@
+const Commando = require('discord.js-commando'),
+    {RichEmbed} = require('discord.js'),
+    util = require('./../../utils'),
+    moment = require('moment');
+
+module.exports = class ClanCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'clan',
+            aliases: [],
+            group: 'clan',
+            memberName: 'clan',
+            description: 'Get clan details',
+            examples: ['clan maximized'],
+            args: [
+                {
+                    key: 'name',
+                    prompt: 'Clan name to find',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    async run(message, {name}) {
+        name = name.trim();
+        let api = `/api/clans/${name}/members`;
+        util.request.api(api).then(result => {
+            message.embed(this.createEmbed(result, name));
+        }).catch(err => {
+            console.log('err', err);
+        });
+    }
+
+    createEmbed(members, name) {
+        let embed = new RichEmbed()
+            .setAuthor(name.toTitleCase(), ``)
+            .setTimestamp()
+            .setThumbnail(`http://services.runescape.com/m=avatar-rs/${name}/clanmotif.png`);
+        let xpCount = 0;
+        for (let i in members) {
+            let member = members[i];
+            xpCount += member.exp;
+        }
+        let leaderRank = ['Owner', 'Deputy Owner', 'Overseer'];
+        let clanLeaders = members.filter(m => { return leaderRank.includes(m.rank)});
+        let leaders = [];
+        let leadersStr = '';
+        for (let i in clanLeaders) {
+            let leader = clanLeaders[i];
+            leaders.push(`${leader.player}`);
+            leadersStr += `${leader.player}\n`
+        }
+        embed.addField(`Keys`, leadersStr, false);
+        embed.addField(`Stats`, `Members: **${members.length}**\nTotal: **${xpCount.toLocaleString()} xp**\nAverage: **${Math.floor(xpCount / members.length).toLocaleString()} xp**`, false);
+        return embed;
+    }
+};
