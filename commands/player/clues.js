7d84bfbc66f7191a6a9550d15cd73d17828692d3
axke <ackempf@eagles.usi.edu>
2018-06-07 14:14:29 -0500

    (feat) Added clue scroll count command

---
+++ commands/player/clues.js
@@ -0,0 +1,54 @@
+const Commando = require('discord.js-commando'),
+    {RichEmbed} = require('discord.js'),
+    util = require('./../../utils');
+
+module.exports = class CluesCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'clues',
+            aliases: ['scrolls'],
+            group: 'player',
+            memberName: 'clues',
+            description: 'Get players clue count',
+            examples: ['clues sync'],
+        });
+    }
+
+    async run(message, args) {
+        let username = args;
+
+        let statsApi = `/api/players/${username}/stats`;
+
+        util.request.api(statsApi).then(hiscore => {
+            this.createEmbed(hiscore).then(embed => {
+                message.embed(embed);
+            });
+        }).catch(err => {
+            console.log('err', err);
+        })
+
+    }
+
+    async createEmbed(hiscores) {
+        if (!hiscores || !hiscores.name) {
+            embed.setAuthor('Scapie');
+            return embed.setDescription('Error getting player profile');
+        }
+
+        var ava = `https://secure.runescape.com/m=avatar-rs/'${hiscores.name.trim().replace(/ /g, '%20')}/chat.png?timestamp=${new Date().getTime()}`;
+        let embed = new RichEmbed()
+            .setAuthor(`${hiscores.name}'s clue scroll count`)
+            .setTimestamp()
+            .setThumbnail(ava)
+            .setDescription(
+                `${"Easy:".bold()} ${hiscores.activities.easy_clue_scrolls.score}\n` +
+                `${"Medium:".bold()} ${hiscores.activities.medium_clue_scrolls.score}\n` +
+                `${"Hard:".bold()} ${hiscores.activities.hard_clue_scrolls.score}\n` +
+                `${"Elite:".bold()} ${hiscores.activities.elite_clue_scrolls.score}\n` +
+                `${"Master:".bold()} ${hiscores.activities.master_clue_scrolls.score}`
+            )
+
+        return embed;
+    }
+
+};
