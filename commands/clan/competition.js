0bdb75d464a38cd48206038db79ebb0980cbbbfd
axke <ackempf@eagles.usi.edu>
2018-06-13 13:17:06 -0500

    maybe last oops

--- commands/clan/competition.js
+++ commands/clan/competition.js
@@ -33,7 +33,7 @@ module.exports = class CompetitionCommand extends Commando.Command {
     }
 
     createEmbed(clan, competition, standings) {
-        const duration = moment.duration((new Date(competition.endDate) - new Date(competition.startDate))*1000, 'milliseconds').asHours();
+        const duration = Math.round(moment.duration((new Date(competition.endDate) - new Date(competition.startDate))*1000, 'milliseconds').asHours() * 100) / 100;
         let embed = new RichEmbed()
             .setAuthor(`${competition.name.toTitleCase()}`, ``)
             //.setTimestamp()
--- commands/misc/about.js
+++ commands/misc/about.js
@@ -29,7 +29,7 @@ module.exports = class AboutCommand extends Commando.Command {
             .addField('Bot Stats',
                 `Guild Count: ${Object.keys(this.client.guilds).length}\n` +
                 `Ping: ${this.client.ping}\n` +
-                `Uptime: ${Matho.round(moment.duration(this.client.uptime, 'milliseconds').asHours() * 100) / 100} hours`,
+                `Uptime: ${Math.round(moment.duration(this.client.uptime, 'milliseconds').asHours() * 100) / 100} hours`,
                 false);
         return embed;
     }
