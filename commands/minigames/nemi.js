f9d7947f70b0a97ffe51123e1ab8e7fc946c2bc6
Alex Kempf <ackempf@eagles.usi.edu>
2019-05-22 20:03:44 -0500

    Update nemi.js

--- commands/minigames/nemi.js
+++ commands/minigames/nemi.js
@@ -26,12 +26,12 @@ module.exports = class NemiCommand extends Commando.Command {
     createEmbed(result) {
         const post = result.data.children[0];
         const flair = post.data.link_flair_text;
+        const img = encodeURI(post.data.preview.images[0].source.url);
         let embed = new RichEmbed()
             .setAuthor(post.data.author, ``)
             .setTimestamp()
             .setDescription(`${flair !== null ? post.data.link_flair_text.mdbold().toUpperCase() + ' ': ''}${post.data.title}`)
-            .setImage(`https://preview.redd.it/s0znpnemrsz21.png?auto=webp&s=f751b0994d9b80794cb4102d354f5ba1c72699e4`);
-            // `${post.data.preview.images[0].source.url}`
+            .setImage(img);            
         return embed;
     }
 };
